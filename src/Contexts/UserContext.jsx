4a39f4212a1d09e8213a992cd9ee729a943b6d23
imrulkaisar <imrulkaisar3@gmail.com>
2023-10-18 08:53:31 +0600

    UserContext is created with firebase authentication

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -1,15 +1,73 @@
-import { createContext, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Authentication/firebase.config";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext(null);
 
-const UserContextProvider = () => {
+const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  // Sign up with email and password
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Login using email and password
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Login with Google account
+  const googleSignIn = () => {
+    setLoading(true);
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  // Logout
+  const logOut = () => {
+    setLoading(true);
+    signOut(auth).then((res) => {
+      const user = res.user;
+      console.log(user);
+      navigate("/login");
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("Current User: ", currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const authInfo = {
     user,
+    loading,
+    signUp,
+    login,
+    googleSignIn,
+    logOut,
   };
 
-  return <UserContext.Provider value={authInfo}></UserContext.Provider>;
+  return (
+    <UserContext.Provider value={authInfo}>{children}</UserContext.Provider>
+  );
 };
 
 export default UserContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import MainRouters from "./Routers/MainRouters";
+import UserContextProvider from "./Contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={MainRouters} />
+    <UserContextProvider>
+      <RouterProvider router={MainRouters} />
+    </UserContextProvider>
   </React.StrictMode>
 );
